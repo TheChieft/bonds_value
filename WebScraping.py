109d3aed86e3f6ddc8e5b81612b43803c14a4e25
aandresfelipeyanezz@gmail.com <aandresfelipeyanezz@gmail.com>
2024-01-29 14:40:25 -0500

    Add All info function -BUG: Algunas funciones no sirven cuando no hay datos que mostrar
    Co-authored-by: Juan David Bocanegra <juan1031@users.noreply.github.com>
    Co-authored-by: Jorfanv <Jorfanv@users.noreply.github.com>

--- WebScraping.py
+++ WebScraping.py
@@ -1,14 +1,12 @@
-# Webscraping to get the value of the bonds of the Colombia Government
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-from selenium import webdriver
-
-import datetime
-import os
 import time
-import sys
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
+# CONSTANTS
 BONDS_PUBLIC = 'https://www.bvc.com.co/mercado-local-en-linea?tab=renta-fija_deuda-publica-segmento-publico'
 BONDS_PRIVATE = 'https://www.bvc.com.co/mercado-local-en-linea?tab=renta-fija_deuda-publica-segmento-privado'
 BONDS_CORPORATE = 'https://www.bvc.com.co/mercado-local-en-linea?tab=renta-fija_deuda-corporativa'
@@ -18,42 +16,38 @@ BOND_INFO_URL_PUBLIC = 'https://www.bvc.com.co/renta-fija-deuda-publica-segmento
 BOND_INFO_URL_PRIVATE = 'https://www.bvc.com.co/renta-fija-deuda-publica-segmento-privado/'
 BOND_INFO_URL_CORPORATE = 'https://www.bvc.com.co/renta-fija-deuda-corporativa/'
 
+OPTS = webdriver.ChromeOptions()
+OPTS.add_argument(
+        "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+OPTS.add_argument("--headless")
+DRIVER = webdriver.Chrome(service=webdriver.chrome.service.Service(
+        ChromeDriverManager().install()))
+
+# FUNCTIONS
 
 # Web Scraping to get information of the bonds
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
-
-
 
 def actual_bonds(url):
-    driver = webdriver.Chrome()
-
     try:
         # Abrir la página con Selenium
-        driver.get(url)
-
+        DRIVER.get(url)
         # Esperar unos segundos para que la página se cargue completamente (puedes ajustar este tiempo según sea necesario)
         time.sleep(5)
-
-        # buscar el elemento tabla
-        table = driver.find_element(By.TAG_NAME, "table")  
-        
-        # Verificar si la tabla se encontró correctamente
+        # search the element in the table 
+        table = DRIVER.find_element(By.TAG_NAME, "table")  
         if table:
             # Obtener todas las filas de la tabla
-            rows = table.find_elements(By.TAG_NAME, 'tr')
-            df = pd.DataFrame(columns = ['Emisor','Nemotécnico','Clase de titulo', 'Fecha de vencimiento'])            
-            for row in rows:
+            rows = table.find_elements(By.TAG_NAME, 'tr')     
+            df = pd.DataFrame(columns = ['Emisor','Nemotécnico','Clase de título', 'Fecha de vencimiento'])            
+            for row in rows: # 
                 try:
                     df.loc[len(df)]= row.text.split("\n")
                 except:
                     df = df.drop(index=0)
                     continue
-            print(df)
     finally:
         # Cerrar el navegador de Selenium al finalizar
-        driver.quit()
+        DRIVER.quit()
         return df
     
 def read_bonds_info(codigo: str):
@@ -69,25 +63,24 @@ def read_bonds_info(codigo: str):
         print("Leyendo archivo")
         data = file.readlines()
         for line in data:
-            if line in keywords: # no entra aquí
+            if line in keywords: 
                 l.append(data[data.index(line) + 1].replace('\n', ''))
         df.loc[len(df)]= l
     return df
 
 
 def get_info_bonds(codigo: str, url: str):
-    driver = webdriver.Chrome()
+    
     url = url + codigo + "?tab=resumen"
     
     try:
         # Abrir la página con Selenium
-        driver.get(url)      
+        DRIVER.get(url)      
         # Esperar unos segundos para que la página se cargue completamente (puedes ajustar este tiempo según sea necesario)
         time.sleep(5)
         # Datos de tablas 
         try:
-            # buscar clase "sc-bXCLTC hetshd"
-            content = driver.find_element(By.CLASS_NAME, "Summarystyled__StyledSummary-sc-1ougflf-0")
+            content = DRIVER.find_element(By.CLASS_NAME, "Summarystyled__StyledSummary-sc-1ougflf-0")
             # guardar archivo en txt
             with open("data/raw/test.txt", "w") as file:
                 file.write(content.text)
@@ -97,12 +90,18 @@ def get_info_bonds(codigo: str, url: str):
         print("DOESN'T WORK the link")
     finally:
         # Analisis  txt
-        df_= read_bonds_info()
+        df_= read_bonds_info(codigo)
         # escribir linea en csv
         df_.to_csv("data/raw/bonds.csv", mode='a', header=False)
         # Cerrar el navegador de Selenium al finalizar
-        driver.quit()
+        DRIVER.quit()
         
+def do_the_scraping():
+    # download actual bonds
+    for url in BONDS:
+        df = actual_bonds(url)
+        for codigo in df['Nemotécnico']:
+            get_info_bonds(codigo, url)
+    
 
-#get_info_bonds("TFIT21280542", BOND_INFO_URL_PUBLIC)
-actual_bonds(BONDS_PUBLIC)
+get_info_bonds("TES UVR 2024", BOND_INFO_URL_PUBLIC)
