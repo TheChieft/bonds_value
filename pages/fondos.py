89ee76b827ed8cb4af21c8535e88b932de558418
juan1031 <jdbv2000@gmail.com>
2024-02-11 10:58:09 -0500

    .

--- pages/fondos.py
+++ pages/fondos.py
@@ -16,14 +16,3 @@ for fund_symbol in fund_list:
     print(f"Símbolo: {fund_symbol}")
     print(f"Tipo de Activo: {fund.info['category']}")
     print("-----------------------------")
-
-    # Muestra las tarjetas de métricas solo para el ticker seleccionado
-# if ticker == selected_stock:
-    # st.header(f'{ticker}')
-#    col1, col2, col3 = st.columns(3)
-#    col1.metric('Market Ticker:', value=market_ticker)
-#    col2.metric('Expected Return:', value=expected_return)
-#    col3.metric('Beta', value=beta)
-#    style_metric_cards(background_color='rgba(0,0,0,0)', border_left_color="#003C6F",
-#                        border_color="#003C6F", box_shadow="blue")
-#    st.write("----")
