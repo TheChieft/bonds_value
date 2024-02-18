f507636a79615ad49d9d369c5ff658cb87b0d5ab
Juan David Bocanegra <98434713+juan1031@users.noreply.github.com>
2024-02-18 13:25:52 -0500

    Update funcionesAcciones.py

--- src/funcionesAcciones.py
+++ src/funcionesAcciones.py
@@ -6,15 +6,27 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import streamlit as st
 import datetime
+import statsmodels.api as sm
+from urllib3.exceptions import MaxRetryError, NewConnectionError
 
 # Funciones para obtener datos de precios de acciones
-def get_stock_data(ticker, start_date, end_date):
-    data = yf.download(ticker, start=start_date,
-                       end=end_date, interval='1mo')['Adj Close']
-    data_df = pd.DataFrame(data)
-    # Asignar el nombre de la columna como una lista
-    data_df.columns = [ticker]
-    return data_df
+
+
+def get_stock_data(ticker, start_date, end_date, max_retries=3):
+    for _ in range(max_retries):
+        try:
+            data = yf.download(ticker, start=start_date,
+                               end=end_date, interval='1mo')['Adj Close']
+            data_df = pd.DataFrame(data)
+            # Asignar el nombre de la columna como una lista
+            data_df.columns = [ticker]
+            return data_df
+        except (MaxRetryError, NewConnectionError) as e:
+            print(f"Error de conexión: {e}")
+            print("Reintentando...")
+    raise Exception(
+        f"No se pudo descargar los datos después de {max_retries} intentos")
+
 
 def get_stock_cumulative_returns(ticker, start_date, end_date):
     stock_prices = get_stock_data(ticker, start_date, end_date)
@@ -26,163 +38,132 @@ def get_stock_cumulative_returns(ticker, start_date, end_date):
     return stock_cumulative_returns
 
 
-def calculate_capm(ticker, market_ticker, start_date, end_date):
+def descargar_datos(ticker, start_date, end_date, max_retries=3):
+    for _ in range(max_retries):
+        try:
+            data = yf.download(ticker, start=start_date, end=end_date, interval='1mo')[
+                'Adj Close'].reset_index()
+            return data
+        except (MaxRetryError, NewConnectionError) as e:
+            print(f"Error de conexión: {e}")
+            print("Reintentando...")
+    raise Exception(
+        f"No se pudo descargar los datos después de {max_retries} intentos")
 
-    stock_returns = get_stock_data(ticker, start_date, end_date)
-    market_returns = get_stock_data(market_ticker, start_date, end_date)
 
-    # Calcular rendimientos logarítmicos
-    stock_returns = np.log(stock_returns / stock_returns.shift(1))
-    market_returns = np.log(market_returns / market_returns.shift(1))
+def calcular_alpha_beta(start_date, end_date, acciones_ticker, mercado_ticker=['^GSPC'], bono_ticker='^TNX'):
+    resultados_dict = {}
+    mercado_data = descargar_datos(mercado_ticker, start_date, end_date)
+    bono_data = descargar_datos(bono_ticker, start_date, end_date)
 
-    alpha_jensen = None
-    beta_value = None
-    regression_data = None
+    for accion_ticker in acciones_ticker:
+        try:
+            # Descargar datos de Yahoo Finance
+            accion_data = descargar_datos(accion_ticker, start_date, end_date)
 
-    X = market_returns[1:]  # Excluir el primer NaN
-    y = stock_returns[ticker][1:]  # Excluir el primer NaN
-    X = sm.add_constant(X)  # Añadir constante para el término alpha
+        # Resto del código para procesar los datos...
 
-    # Estimar CAPM: rendimiento_stock ~ alpha + beta * rendimiento_benchmark
-    model = sm.OLS(y, X, missing='drop').fit()
-    alpha_jensen = model.params[0]  # Alpha de Jensen
-    beta_value = model.params[1]   # Beta
+        except (MaxRetryError, NewConnectionError) as e:
+            st.warning(f"Error de conexión para {accion_ticker}: {e}")
+            st.warning("Por favor, recarga la página e intenta nuevamente.")
+            st.stop()  # Detiene la ejecución de la aplicación de Streamlit
 
-    # Almacenar datos de la regresión para la gráfica
-    regression_data = {
-        'X': X['const'].tolist(), 'y': model.fittedvalues.tolist()}
+        except Exception as e:
+            st.warning(f"Error al procesar {accion_ticker}: {e}")
+            st.warning("Por favor, recarga la página e intenta nuevamente.")
+            st.stop()  # Detiene la ejecución de la aplicación de Streamlit
 
-    return alpha_jensen, beta_value, regression_data, model.summary()
+        # Calcular rendimientos logarítmicos
+        rendimientos_accion = np.log(
+            accion_data['Adj Close'] / accion_data['Adj Close'].shift(1)).dropna()
+        rendimientos_mercado = np.log(
+            mercado_data['Adj Close'] / mercado_data['Adj Close'].shift(1)).dropna()
 
-def calculate_stock_returns(stocks_list = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA'], indice = ['^GSPC']):
-    # Getting the stock data from yfinance
-    data = yf.download(stocks_list + indice, period='5y',interval='1mo')['Adj Close']
-    indice = indice[0]
-    # Calculating Daily % change in stock prices
-    daily_returns = data.pct_change()
-    daily_returns.iloc[0,:] = 0
+        # Calcular tasa mensual para el bono
+        def tasa_mensual(x):
+            return (1 + (x / 100)) ** (1 / 12) - 1
 
-    # Boxplot of daily returns (in %)
-    #daily_returns.boxplot(figsize=(6, 5), grid=False)
+        tasa_libre_riesgo = pd.DataFrame(
+            {'tasa_mensual': bono_data['Adj Close'].iloc[1:].apply(tasa_mensual)})
 
-    # Initializing empty dictionaries to save results
+        # Calcular excesos de retornos
+        accion_er = rendimientos_accion - tasa_libre_riesgo['tasa_mensual']
+        marcado_er = rendimientos_mercado - tasa_libre_riesgo['tasa_mensual']
 
-    beta,alpha = dict(), dict()
+        # Concatenar las series de excesos de retornos
+        df_excesos = pd.concat([accion_er, marcado_er], axis=1).dropna()
+        df_excesos.columns = ['accion_er', 'marcado_er']
 
-    # Make a subplot
+        # Agregar una columna de unos para representar el intercepto
+        df_excesos['intercepto'] = 1
+        # Ajustar el modelo de regresión lineal
+        modelo = sm.OLS(df_excesos['accion_er'],
+                        df_excesos[['marcado_er', 'intercepto']])
 
-    # fig, axes = plt.subplots(1,3, dpi=150, figsize=(15,8))
+        resultados = modelo.fit()
 
-    # axes = axes.flatten()
+        # Obtener los coeficientes del modelo
+        coeficientes = resultados.params
+        alpha, beta = coeficientes['intercepto'], coeficientes['marcado_er']
 
-    # for idx, stock in enumerate(daily_returns.columns.values[:-1]):
-
-        # scatter plot between stocks and the NSE
-
-    #    daily_returns.plot(kind = "scatter", x = indice[0], y = stock, ax=axes[idx])
-
-        # Fit a line (regression using polyfit of degree 1)
-
-    #    b_, a_ = np.polyfit(daily_returns[indice[0]], daily_returns[stock], 1)
-
-    #    regression_line = b_ * daily_returns[indice[0]] + a_
-
-    #    axes[idx].plot(daily_returns[indice[0]], regression_line, "-", color = "r")
-
-        # save the regression coeeficient for the current stock
-
-    #    beta[stock] = b_
-    #    alpha[stock] = a_
-
-    # plt.show()
-
-    # ----------------- PRUEBA CON GO ------------------------------
+        resultados_dict[accion_ticker] = {'alpha': alpha, 'beta': beta}
 
     fig = go.Figure()
 
-    for idx, stock in enumerate(daily_returns.columns.values[:-1]):
-        # Scatter plot entre las acciones y el índice
-        fig.add_trace(go.Scatter(x=daily_returns[indice], y=daily_returns[stock],
-                                 mode='markers', name=stock))
+    colores_pastel = ['rgb(255, 223, 186)', 'rgb(173, 216, 230)',
+                      'rgb(144, 238, 144)', 'rgb(221, 160, 221)', 'rgb(255, 165, 0)']
 
-        # Ajustar una línea de regresión (usando polyfit de grado 1)
-        b_, a_ = np.polyfit(daily_returns[indice], daily_returns[stock], 1)
-        regression_line = b_ * daily_returns[indice] + a_
+    for accion_ticker, valores in resultados_dict.items():
+        # Valores de beta para trazar la línea
+        beta_values = np.linspace(0, max(valores['beta'], 4), 100)
 
-        # Añadir la línea de regresión
-        fig.add_trace(go.Scatter(x=daily_returns[indice], y=regression_line,
-                                 mode='lines', name=f'Regresión - {stock}', line=dict(color='red')))
+        # Calcula la línea CAPM
+        SML_values = valores['alpha'] + beta_values * \
+            (rendimientos_mercado.mean() -
+             tasa_libre_riesgo['tasa_mensual'].mean())
 
-        # Guardar los coeficientes de regresión para la acción actual
-        beta[stock] = b_
-        alpha[stock] = a_
+        # Usa el operador de módulo para obtener el índice correcto del color
+        selected_color = colores_pastel[len(fig.data) % len(colores_pastel)]
 
-    # Configurar el diseño de la figura
-    fig.update_layout(
-        autosize=True,
-        title=dict(
-            text='Retornos Acumulados',
-            font=dict(color='white', size=20),
-            x=0.35,
-            y=0.9
-        ),
-        legend=dict(font=dict(color='white')),
-        paper_bgcolor='rgba(0,0,0,0)',
-        plot_bgcolor='rgba(0,0,0,0)',
-        xaxis_showgrid=True,
-        yaxis_showgrid=True,
-        xaxis_ticks='outside',
-        yaxis_ticks='outside',
-        xaxis_linecolor='white',
-        yaxis_linecolor='white',
-        showlegend=True,
-        xaxis_gridcolor='rgba(255, 255, 255, 0.1)',
-        yaxis_gridcolor='rgba(255, 255, 255, 0.1)',
-        margin=dict(l=0, r=50, b=50, t=50),
-    )
+        # Agrega la línea CAPM
+        fig.add_trace(go.Scatter(x=beta_values, y=SML_values, mode='lines', line=dict(
+            color=selected_color, width=2), name=f'{accion_ticker} - SML', hoverlabel=dict(font=dict(color='white')), hovertext=[f'{accion_ticker} - SML']*len(beta_values)))
 
-    # Mostrar el gráfico interactivo en Streamlit
-    #st.plotly_chart(fig)
+        # Agrega el punto de intersección (alpha) sin leyenda
+        fig.add_trace(go.Scatter(x=[valores['beta']], y=[valores['alpha'] + valores['beta'] * (rendimientos_mercado.mean() - tasa_libre_riesgo['tasa_mensual'].mean())],
+                                 mode='markers', marker=dict(color='blue' if valores['alpha'] < 0 else 'red', size=8), name=f'{accion_ticker} - Rentabilidad Esperada', hoverlabel=dict(font=dict(color='white'))))
 
-    keys = list(beta.keys()) # list of stock names
+        # Ajusta los ejes x e y
+        fig.update_xaxes(tickcolor='white', tickfont=dict(
+            color='white'), title=dict(text='Beta (Riesgo Sistemático)', font=dict(color='white')), dtick=0.4)
+        fig.update_yaxes(tickcolor='white', tickfont=dict(
+            color='white'), title=dict(text='Rentabilidad Esperada', font=dict(color='white')), dtick=0.02)
 
-    beta_3 = dict()
+        # Ajusta el título general
+        fig.update_layout(
+            title=dict(
+                text='CAPM - Capital Asset Pricing Model',
+                font=dict(color='white', size=20),
+                x=0.1,
+                y=0.9  # Ajusta la posición del título
+            ),
+            # showlegend=True,
+            legend=dict(font=dict(color='white')),
+            width=700,
+            height=500,
+            paper_bgcolor='rgba(0,0,0,0)',  # Fondo del papel transparente
+            plot_bgcolor='rgba(0,0,0,0)',   # Fondo del gráfico transparente
+            xaxis_showgrid=True,
+            yaxis_showgrid=True,
+            xaxis_ticks='outside',
+            yaxis_ticks='outside',
+            xaxis_linecolor='white',
+            yaxis_linecolor='white',
+            # showlegend=True,
+            # Color de la cuadrícula del eje x con alpha
+            xaxis_gridcolor='rgba(255, 255, 255, 0.1)',
+            # Color de la cuadrícula del eje x con alpha
+            yaxis_gridcolor='rgba(255, 255, 255, 0.1)'
+        )
 
-    for k in keys:
-        beta_3[k] = [daily_returns[[k,indice]].cov()/daily_returns[indice].var()][0].iloc[0,1]
-        print(f"el beta de {k}: {beta[k]}")
-
-    ER = dict()
-
-    end_date = datetime.datetime.now()
-    start_date = end_date - datetime.timedelta(days=5*365)
-
-    rf_data = yf.download('^TNX', start=start_date, end=end_date)
-    # Calcular el rendimiento promedio de los bonos del Tesoro a 10 años
-    rf = rf_data['Adj Close'].pct_change().mean() * 250  # Suponiendo 250 días de trading al año
-
-    trading_days = 250
-
-    # Estimate the expected return of the market using the daily returns
-
-    rm = daily_returns[indice].mean() * trading_days
-
-    for k in keys:
-
-        # Calculate return for every security using CAPM
-
-        ER[k] = rf + beta[k] * (rm-rf)
-
-    for k in keys:
-
-        print("Expected return based on CAPM model for {} is {}%".format(k, round(ER[k], 2)))
-
-    # Calculating historic returns
-
-    for k in keys:
-
-        print('Return based on historical data for {} is {}%'.format(k, round(daily_returns[k].mean() * trading_days, 2)))
-
-    historic_returns = {k: round(daily_returns[k].mean() * trading_days, 2) for k in keys}
-
-    return fig, beta, alpha, historic_returns
+    return resultados_dict, fig
